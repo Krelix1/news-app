0ebba8cb809b22986b54dd761f08bd3def0b9972
Krelix1 <kreksalix@gmail.com>
2019-11-15 15:24:30 +0300

    Create project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import css from '../../index.css';
+
+class App extends React.Component {
+    render() {
+        return <div className={css.hello}>Hello World</div>
+    }
+}
+export default App;
